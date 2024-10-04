d47089ba385be7ff19d4be70418e278a1e85ba51
Your Name <khalidsaifullaha3@gmail.com>
2024-10-04 22:25:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const loadAllPhons = async(status, brandName)=>{
+    // console.log(brandName);
+    document.getElementById("spinner").style.display = "none";
+
+    // fetch(" https://openapi.programming-hero.com/api/phones?search=iphone")
+    // .then(res => res.json())
+    // .then(data => console.log(data))
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName?brandName :"iphone"}`);
+    const data = await response.json();
+    console.log(data);
+    if(status){
+        displayAllPhone(data.data)
+    }
+    else{
+        displayAllPhone(data.data.slice(0,6));
+    }
+    
+    
+}
+// {
+//     "brand": "Apple ",
+//     "phone_name": "iPhone 13 mini",
+//     "slug": "apple_iphone_13_mini-11104",
+//     "image": "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg"
+// }
+const displayAllPhone = (phones) => {
+    // document.getElementById("phones-container").innerHTML="";
+    
+    const phoneContainer = document.getElementById("phones-container");
+   phones.forEach(phone => {
+    const {brand, image, slug} = phone;
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card bg-base-100 w-96 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img
+      src=${image}
+      alt="Shoes"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">${brand}</h2>
+    <p>${slug}</p>
+    <div class="card-actions">
+      <button onclick = "phoneDetails('${slug}')" class="btn btn-primary">Show Details</button>
+    </div>
+  </div>
+</div>
+    `;
+    phoneContainer.appendChild(div);
+   });
+}
+
+const handleShowAll=() =>{
+    loadAllPhons(true);
+}
+const handelSearch = ()=>{
+    document.getElementById("spinner").style.display = "block";
+    const searchText=document.getElementById("search-box").value;
+    setTimeout(function (){
+        loadAllPhons(false,searchText)
+    }, 2000)
+}
+
+const phoneDetails =async(slugs) => {
+    const response =await fetch(` https://openapi.programming-hero.com/api/phone/${slugs}`);
+    const data = await response.json();
+    console.log(data.data);
+
+    const {brand, image, slug} = data.data;
+    const modalContainer = document.getElementById('modal-container');
+
+    modalContainer.innerHTML = `
+    <dialog id="my_modal_1" class="modal">
+    <div class="modal-box">
+    <img src = ${image}>
+      <h3 class="text-lg font-bold">${brand}</h3>
+      <p class="py-4">${slug}</p>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+    `
+    my_modal_1.showModal()
+
+}
+
+loadAllPhons(false, "iphone" )
